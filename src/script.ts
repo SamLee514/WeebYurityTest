1afa04fe233ba5abf0c5d7ef817dd142b263d21d
SamLee514 <samuel.r.lee@vanderbilt.edu>
2020-07-19 05:36:15 -0400

    INITIAL COMMIT Y33T

---
+++ src/script.ts
@@ -0,0 +1,204 @@
+// import * as data from './questions.json';
+
+const questions = {
+    "title":"Questions",
+    "description":"List of questions for the test",
+    "questions":[
+        "Do you know what anime is?",
+        "Have you ever watched anime?",
+        "Have you ever watched anime outside of 4KidsTV, AdultSwim, Netflix or Hulu?",
+        "Have you ever completed an anime that has more than 200 episodes?",
+        "Have you ever read a manga or light novel?",
+        "Do you pronounce manga m-AH-nga instead of m-AY-nga?",
+        "Have you ever completed a manga series that has more than 70 volumes?",
+        "Have you watched or read more than 10 anime and/or manga?",
+        "Have you ever watched or read the same anime or manga more than 4 times?",
+        "Have you ever cried because of an anime or manga?",
+        "Have you ever considered an anime or manga character to be your role model?",
+        "Have you ever watched anime in Japanese without being able to speak it natively?",
+        "Do you prefer watching anime in Japanese even if you don't speak it natively?",
+        "Have you ever intentionally watched the Japanese dub for a show/movie that was originally in English?",
+        "Have you ever judged someone else for their taste in anime?",
+        "Have you ever judged someone else for choosing to watch English-dubbed anime?",
+        "Have you ever not paid attention in a class or meeting because you were secretly reading manga?",
+        "Have you ever bonded with someone solely over a mutual love of anime and manga?",
+        "Have you ever made a reference to an anime or manga in a room where at most 2 people would get it?",
+        "Have you ever played a dating sim?",
+        "Have you ever listened to J-Pop or an anime opening/ending?",
+        "Have you ever learned to play J-Pop or anime music on an instrument?",
+        "Have you ever learned the words to a J-Pop/anime song?",
+        "Bonus point if you still don't know what the words mean",
+        "Have you ever used an anime character as your profile picture?",
+        "Have you ever used an anime-related image as the background on one of your devices?",
+        "Are you a member of any anime/manga-related meme pages, subreddits, etc.?",
+        "Have you ever used anime or manga as a conversation starter with a stranger?",
+        "Have you ever attempted to learn a dance that was featured in an anime?",
+        "Have you ever taken up an activity (such as volleyball or piano) because you were inspired by an anime or manga?",
+        "Have you ever included an anime OP/ED/OST in your workout playlist?",
+        "Have you ever said words like 'Nani', 'Baka' and 'Sugoi' out loud around people outside of an explicitly Japanese context (e.g. a Japanese class or while conversing in Japanese?)",
+        "Are those words part of your regular vocabulary?",
+        "Have you ever referred to anyone (not in an appropriate context) as 'senpai', 'onii-chan' or 'onee-chan'?",
+        "Have you ever answered the phone with 'moshi moshi' or 'hai'?",
+        "Have yuwu evew typed wike dis (OwO)?",
+        "Have you ever appended cat noises to the end of your sentences ~nyaan~",
+        "Are you subscribed to a streaming service primarily for the anime they have?",
+        "Is most of the content you consume in a language you don't speak natively?",
+        "Have you ever attempted to (or succeeded in) learning Japanese because of the content you consume?",
+        "Have you ever taken a formal course (Duolingo doesn't count) in Japanese because of the content you consume?",
+        "Have you ever majored or minored in Japanese because of the content you consume?",
+        "Have you ever visited Japan primarily because of the content you consume?",
+        "Have you ever moved to Japan primarily because of the content you consume?",
+        "Have you ever run away from your responsibilities or wished you could stop time so you could watch more anime?",
+        "Have you ever taken a stand against someone for criticizing anime?",
+        "Oof were those last two questions JoJo references?",
+        "Do you know more historical figures/mythological beings through anime/manga representations of them than actual historical figures/mythological beings?",
+        "Do you have a waifu?",
+        "Have you ever referred to someone else's waifu as 'trash'?",
+        "Do you understand the terms 'tsundere' and 'yandere'?",
+        "Have you ever been to an anime-related convention?",
+        "Have you been to an anime-related convention more than 5 times?",
+        "Have you ever had to travel by plane to attend an anime-related convention?",
+        "Have you ever spent more than $100 at an anime-related convention (ticket not included)?",
+        "Have you ever cosplayed?",
+        "Have you ever spent more than 6 hours and/or $100 on your cosplay?",
+        "Have you ever cosplayed competitively?",
+        "Have you ever worn your cosplay in public outside of an anime-related event, or walking to/from that event?",
+        "Have you ever Naruto run in public?",
+        "Have you ever purchased a physical copy of an anime, manga or light novel?",
+        "Have you ever purchased an anime poster?",
+        "Have you ever purchased an anime figurine or plush toy?",
+        "Do you own more than 5 anime figurines or plush toys?",
+        "Do you own any figurines depicting a character of the opposite sex?",
+        "Do you own any figures you would be compelled to keep hidden from minors?",
+        "At this point, is your wall more anime poster than wall?",
+        "Do you have a display case/shelf/cabinet dedicated solely for anime figurines?",
+        "Are most of the decorations in your house related to anime or manga?",
+        "Have you ever purchased a body pillow?",
+        "Does all the anime or manga-related merchandise you own total more than $500?",
+        "Do you own a katana?",
+        "Have you ever visited a maid cafe?",
+        "Have you ever gone a week or more without a shower or bath?",
+        "Have you ever binged an entire anime or manga series in a day?",
+        "Are you a virgin?",
+        "Do you legitimately prefer 2D over 3D women/men?",
+        "Have you ever willingly been exposed to hentai?",
+        "Have you ever consumed any doujins featuring sexual activity?",
+        "Do you understand any of the following terms: 'NTR', 'futa', 'ahegao', 'nakadashi', 'yaoi', 'yuri', 'kemonomimi', 'gyaru', 'loli',  or 'shota'?",
+        "Do you understand more than half of those terms?",
+        "Do you understand ALL of the above terms?",
+        "Are you attracted to lolis, shotas, traps and/or reverse traps?",
+        "Does the number 177013 mean anything to you?",
+        "Have you ever done something involving anime/manga for a school project?",
+        "Have you ever made a video essay about an anime/manga?",
+        "Have you ever made a translation for a manga or a sub for an anime?",
+        "Have you ever made a dub for an anime?",
+        "Have you ever done any official work for an anime or manga?",
+        "Have you ever made a compilation video of a seiyuu's work?",
+        "Have you ever intentionally spent the night at an internet cafe?",
+        "Have you ever intentionally spent more than a week in a cyber-cafe?",
+        "Do you currently intentionally live in a cyber-cafe?",
+        "Do you unironically consider yourself a NEET or a hikikomori, or has anyone else ever called you that?",
+        "Do you prefer to shun real human contact in favor of consuming anime/manga?",
+        "Have you ever seriously considered jumping in front of a truck so you'll be reincarnated as a fantasy/video-game hero?",
+        "Have you ever stalked a seiyuu, mangaka or anime director?",
+        "Have you ever blackmailed a seiyuu, mangaka or anime director?",
+        "Have you ever kidnapped a seiyuu, mangakka or anime director?",
+        "Are you a professional seiyuu, mangaka or anime director?"
+
+        // There should be text right before the submit button like "your next line is: calculate my scores!"
+    ]
+}
+
+function randomDateRange(start, end) {
+    const mlist = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+    const date1 = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    const monthNum1 = date1.getMonth();
+    const month1 = mlist[monthNum1];
+    const year1 = date1.getFullYear();
+
+    const date2 = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    const monthNum2 = date1.getMonth();
+    const month2 = mlist[monthNum2];
+    const year2 = date2.getFullYear();
+
+    return `${month1} ${year1} - ${month2} ${year2}`
+}
+
+
+var count = 1;
+var score = 9.23;
+questions.questions.map(question => {
+    // $(`<p><input type="checkbox">${question}</input></p>`).appendTo('#checklist'); 
+    var color;
+    if (count % 2 == 1) {
+        color = 'white'
+    } else {
+        color = '#f6f6f6'
+    }
+
+    // Decorative nonsense
+    const owos = `OWO (${Math.floor(Math.random() * Math.floor(150))} onee-chans)`;
+    const dates = randomDateRange(new Date(2012, 0, 1), new Date);
+    // Splitting x,xxx,xxx format into 3 because I'm lazy
+    const otakus = Math.floor(Math.random() * Math.floor(999)) + ','
+                 + Math.floor(Math.random() * Math.floor(999)) + ','
+                 + Math.floor(Math.random() * Math.floor(999)) + ' otakus';
+
+    $(`<div class='item' style='background-color: ${color}'>
+        <div class='index'>${count}</div>
+        <div class='question'>
+            ${question}
+            <div class='decoration'>
+                ${owos} <br>
+                ${dates} <br>
+                ${otakus}
+            </div>
+        </div>
+        <div class='score'>
+            <img class='icon' src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Full_Star_Yellow.svg'></img>
+            ${score.toFixed(2)}</div>
+        <div class='checkbox'>
+            <input type='checkbox'></input>
+        </div>
+    </div>`).appendTo('#checklist');
+    count += 1;
+    score -= 0.04;
+});
+
+// $( '#form' ).submit( e => {
+const calculateScore = () => {
+    const score = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    var message;
+    console.log('Your score is:', score)
+    if (score == 0) {
+        message = "Wow really? That's kind of...disappointing. Congrats on your uncorrupted life I guess."
+    } else if (0 < score && score < 10) {
+        message = "You have a long way to go, normie. Go watch the English dub of Bleach or something.";
+    } else if (10 <= score && score < 40) {
+        message = "Welcome to the real world, weeblet. You're about 3 anime conventions and a collectible box set away from the deep end.";
+    } else if (40 <= 80) {
+        message = "Ah, finally a person of culture. You're a comrade to the rest of us, and a reject to polite society. I'd invest in some deoderant soon, by the way.";
+    } else if (80 <= score && score < 95) {
+        message = "Well this is a little concerning. Have you looked into going outside? It's a wonderful 3-dimensional world out there.";
+    } else if (95 <= score && score < 100) {
+        message = "Ok, that's just kind of gross. I'd ask you not to reproduce but it doesn't look like you'd have a shot at that anyway.";
+    } else {
+        message = "FBI? Yes this degenerate right here. Needs to be isekai'd stat.";
+    }
+    $('#results').empty();
+    $(`<div>
+            <h2>Your score is: ${score}</h2>
+            ${message}
+        </div>`).appendTo('#results');  
+    // e.preventDefault();
+};
+
+const clearCheckboxes = () => {
+    $('input[type="checkbox"]:checked').prop('checked',false);
+}
+
+
+
+// const data = $.getJSON('questions.json', question => console.log(question));
+ 
+
